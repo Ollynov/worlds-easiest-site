6f1812fd35548dfec6f3793f94108ff3d08d838f
ollynov <ollynov@gmail.com>
2018-05-22 15:43:02 -0700

    got my app running with dynamic name

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path')
 const app = express()
 var bodyParser = require('body-parser');
 var http = require('http');
+var fs = require('fs');
+
 // var shell = require('shelljs');
 var shell = require('child_process');
 // shell.exec('echo chiiii')
@@ -11,6 +13,13 @@ var shell = require('child_process');
 
 app.use('/', express.static(path.join(__dirname, 'dist')))
 app.use(bodyParser.json());
+
+function buildHtml(name) {
+    var header = 'Demo Header';
+
+    return '<!DOCTYPE html>'
+         + '<html><header>' + header + '</header><body>' + name + '</body></html>';
+};
 
 app.post('/newProfile', (req, res, next) => {
     const name = req.body.name
@@ -20,23 +29,21 @@ app.post('/newProfile', (req, res, next) => {
     //     res.write(`<p>The website of the famous: ${req.body.name}</p>`);
     //     res.end('</body></html>');
     // }).listen(1337);
-    const newApp = express();
-    newApp.get('/', (request, response) => response.status(200).send('new website with: ' + name))
-    newApp.listen(3002, () => console.log('app listening on port 3002!'))
-    //shell.exec('cd /Users/Aljosha.novakovic@ibm.com/Dev/worlds-easiest-site/simpleSite')
-    // shell.exec('cd /Users/Aljosha.novakovic@ibm.com/Dev/worlds-easiest-site/simpleSite', (code, stdout, stderr) => {
-    // shell.exec('cd /Users/Aljosha.novakovic@ibm.com/Dev/worlds-easiest-site/simpleSite', (error, stdout, stderr) => {
-    //     console.log('Exit code:', error);
-    //     console.log('Program output:', stdout);
-    //     console.log('Program stderr:', stderr);
-        
-        
-    //     //shell.exec('up')
-    //     //shell.exec('up url --open')
-    //   });
-    //shell.execSync('echo chiiii')
-    // shell.exec('up')
-    // shell.exec('up url --open')
+    // const newApp = express();
+    // newApp.get('/', (request, response) => response.status(200).send('new website with: ' + name))
+    // newApp.listen(3002, () => console.log('app listening on port 3002!'))
+    
+
+    var fileName = './simpleSite/index.html';
+    var stream = fs.createWriteStream(fileName);
+
+    stream.once('open', function(fd) {
+        var html = buildHtml(name);
+        console.log('our built html: ', html)
+        stream.end(html);
+    });
+
+
     shell.exec("up", {cwd: '/Users/Aljosha.novakovic@ibm.com/Dev/worlds-easiest-site/simpleSite'}, function(error,stdout,stderr){
         console.log('Exit code:', error);
         console.log('Program output:', stdout);
